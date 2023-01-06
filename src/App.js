6e2db10cf990d3357446392e3ded7bf5362fc188
Kevin Le <kevinles117@yahoo.com>
2023-01-05 16:23:41 -0800

    Finished 2.11

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import FilterQuery from './components/FilterQuery.js'
 import PersonForm from './components/PersonForm.js'
 import DisplayPerson from './components/DisplayPerson.js'
 
 const App = () => {  
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setNewFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const handleAddNewPerson = (event) => {
     event.preventDefault()
